4e7ba65db9c7b7b972d2022b9d1023741032cc11
MIShanto <shanto4045@gmail.com>
2022-02-20 13:33:16 +0600

    updated

---
+++ Assets/Scripts/10. Getdatas from player/ANNDrive.cs
@@ -0,0 +1,235 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class ANNDrive : MonoBehaviour
+{
+    ANN2 ann;
+    public float visibleDistance = 200f;
+    public int epochs = 1000;
+    public float moveSpeed = 80.0f;
+    public float rotationSpeed = 120.0f;
+    public double initialAlpha = 0.5;
+
+    bool trainingDone = false;
+    float trainingProgress = 0;
+    double sse = 0; // sum of squares error
+    double lastSSE = 1;
+
+    public float translation;
+    public float rotation;
+
+    public bool loadFromFile = false;
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(25, 25, 250, 30), "SSE: " + lastSSE);
+        GUI.Label(new Rect(25, 40, 250, 30), "Alpha: " + ann.alpha);
+        GUI.Label(new Rect(25, 55, 250, 30), "Trained: " + trainingProgress);
+    }
+    private void Start()
+    {
+        ann = new ANN2(5, 2, 1, 10, initialAlpha);
+
+        //StartCoroutine(LoadTrainingSet());
+
+        if (loadFromFile)
+        {
+            LoadWeightsFromFile();
+            trainingDone = true;
+        }
+        else
+            StartCoroutine(LoadTrainingSet());
+    }
+
+    IEnumerator LoadTrainingSet()
+    {
+        string path = Application.dataPath + "/trainingData.txt";
+        string line;
+
+        if (File.Exists(path))
+        {
+            int lineCount = File.ReadAllLines(path).Length;
+            StreamReader trainingDataFile = File.OpenText(path);
+
+            List<double> calcOutputs = new List<double>();
+            List<double> inputs = new List<double>();
+            List<double> outputs = new List<double>();
+
+            for (int i = 0; i < epochs; i++)
+            {
+                sse = 0;
+                trainingDataFile.BaseStream.Position = 0;
+
+                string currentWeights = ann.PrintWeights();
+
+                while ((line = trainingDataFile.ReadLine()) != null)
+                {
+                    string[] data = line.Split(',');
+
+                    float thisError = 0;
+
+                    if (System.Convert.ToDouble(data[5]) != 0 && System.Convert.ToDouble(data[6]) != 0) //if output values arent zero do action..
+                    {
+                        inputs.Clear();
+                        outputs.Clear();
+
+                        inputs.Add(System.Convert.ToDouble(data[0]));
+                        inputs.Add(System.Convert.ToDouble(data[1]));
+                        inputs.Add(System.Convert.ToDouble(data[2]));
+                        inputs.Add(System.Convert.ToDouble(data[3]));
+                        inputs.Add(System.Convert.ToDouble(data[4]));
+
+                        double output_1 = Map(0, 1, -1, 1, System.Convert.ToSingle(data[5]));
+                        outputs.Add(output_1);
+
+                        double output_2 = Map(0, 1, -1, 1, System.Convert.ToSingle(data[6]));
+                        outputs.Add(output_2);
+
+                        calcOutputs = ann.Train(inputs, outputs);
+
+                        thisError = ((Mathf.Pow((float)(outputs[0] - calcOutputs[0]), 2) +
+                                        Mathf.Pow((float)(outputs[1] - calcOutputs[1]), 2))) / 2.0f;
+                    }
+
+                    sse += thisError;
+                }
+
+                trainingProgress = (float)i / (float)epochs;
+                sse /= lineCount;
+                //lastSSE = sse;
+
+                //We will use preserved weight here which found a decent local optimum. 
+                // we r using it not to increase sse again...
+
+                //if sse isn't better then reload previous weights
+                // and decrease alpha.....
+
+                if (lastSSE < sse)
+                {
+                    ann.LoadWeights(currentWeights);
+                    ann.alpha = Mathf.Clamp((float)ann.alpha - 0.001f, 0.01f, 0.9f);
+                }
+                else // increase alpha
+                {
+                    ann.alpha = Mathf.Clamp((float)ann.alpha + 0.001f, 0.01f, 0.9f);
+                    lastSSE = sse;
+                }
+
+                yield return null;
+            }
+        }
+
+        trainingDone = true;
+        SaveWeightsToFile();
+    }
+
+    void SaveWeightsToFile()
+    {
+        string path = Application.dataPath + "/ANNweights.txt";
+        StreamWriter wf = File.CreateText(path);
+        wf.WriteLine(ann.PrintWeights());
+        wf.Close();
+    }
+
+    void LoadWeightsFromFile()
+    {
+        string path = Application.dataPath + "/ANNweights.txt";
+        StreamReader wf = File.OpenText(path);
+
+        if (File.Exists(path))
+        {
+            string line = wf.ReadLine();
+            ann.LoadWeights(line);
+        }
+    }
+
+    float Map(float newfrom, float newto, float origfrom, float origto, float value)
+    {
+        if (value <= origfrom)
+            return newfrom;
+        else if (value >= origto)
+            return newto;
+        return (newto - newfrom) * ((value - origfrom) / (origto - origfrom)) + newfrom;
+    }
+
+    float Round(float value)
+    {
+        return ((float)System.Math.Round(value, System.MidpointRounding.AwayFromZero) / 2.0f);
+    }
+
+    private void Update()
+    {
+        if (!trainingDone) return;
+
+        float forwardDist = 0, rightDist = 0, leftDist = 0,
+              right45Dist = 0, left45Dist = 0;
+
+        List<double> calcOutputs = new List<double>();
+        List<double> inputs = new List<double>();
+        List<double> outputs = new List<double>();
+
+        //Raycasts
+        RaycastHit hit;
+
+        //forward
+        if (Physics.Raycast(transform.position, this.transform.forward, out hit, visibleDistance))
+        {
+            // we will keep farthest value to small(0) and closest value to big(1)
+            forwardDist = 1 - Round(hit.distance / visibleDistance); // normalized and rounded
+        }
+
+        //right
+        if (Physics.Raycast(transform.position, this.transform.right, out hit, visibleDistance))
+        {
+            // we will keep farthest value to small(0) and closest value to big(1)
+            rightDist = 1 - Round(hit.distance / visibleDistance); // normalized and rounded
+        }
+
+        //left
+        if (Physics.Raycast(transform.position, -this.transform.right, out hit, visibleDistance))
+        {
+            // we will keep farthest value to small(0) and closest value to big(1)
+            leftDist = 1 - Round(hit.distance / visibleDistance); // normalized and rounded
+        }
+
+        //right45
+        if (Physics.Raycast(transform.position, Quaternion.AngleAxis(-45, Vector3.up) * transform.right, out hit, visibleDistance))
+        {
+            // we will keep farthest value to small(0) and closest value to big(1)
+            right45Dist = 1 - Round(hit.distance / visibleDistance); // normalized and rounded
+        }
+
+        //left45
+        if (Physics.Raycast(transform.position, Quaternion.AngleAxis(45, Vector3.up) * -transform.right, out hit, visibleDistance))
+        {
+            // we will keep farthest value to small(0) and closest value to big(1)
+            left45Dist = 1 - Round(hit.distance / visibleDistance); // normalized and rounded
+        }
+
+        inputs.Add(forwardDist);
+        inputs.Add(rightDist);
+        inputs.Add(leftDist);
+        inputs.Add(right45Dist);
+        inputs.Add(left45Dist);
+
+        outputs.Add(0);
+        outputs.Add(0);
+
+        calcOutputs = ann.CalcOutput(inputs, outputs);
+
+        float translationInput = Map(-1, 1, 0, 1, (float)calcOutputs[0]);
+        float rotationInput = Map(-1, 1, 0, 1, (float)calcOutputs[1]);
+
+        translation = translationInput * moveSpeed * Time.deltaTime;
+        rotation = rotationInput * rotationSpeed * Time.deltaTime;
+
+        this.transform.Translate(0, 0, translation);
+        this.transform.Rotate(0, rotation, 0);
+    }
+
+
+
+}
---
+++ Assets/Scripts/10. Getdatas from player/Drive.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Drive : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    public float visibleDistance = 200.0f;
+
+    List<string> collectedTrainingData = new List<string>();
+
+    StreamWriter trainingDataFile;
+    private void Start()
+    {
+        string path = Application.dataPath + "/trainingData.txt";
+        trainingDataFile = File.CreateText(path);
+    }
+
+    private void OnApplicationQuit()
+    {
+        foreach (string trainingData in collectedTrainingData)
+        {
+            trainingDataFile.WriteLine(trainingData);
+        }
+
+        trainingDataFile.Close();
+    }
+
+    float Round(float value)
+    {
+        return ((float) System.Math.Round(value, System.MidpointRounding.AwayFromZero) / 2.0f);
+    }
+    void Update()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translationInput = Input.GetAxis("Vertical");
+        float rotationInput = Input.GetAxis("Horizontal");
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        float translation = Time.deltaTime * speed * translationInput;
+        float rotation = Time.deltaTime * rotationSpeed * rotationInput;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
+
+        Debug.DrawRay(transform.position, this.transform.forward * visibleDistance, Color.red);
+        Debug.DrawRay(transform.position, this.transform.right * visibleDistance, Color.red);
+        Debug.DrawRay(transform.position, -this.transform.right * visibleDistance, Color.red);
+        //look left
+        Debug.DrawRay(transform.position, Quaternion.AngleAxis(45, Vector3.up) * -this.transform.right * visibleDistance, Color.green);
+        //look right
+        Debug.DrawRay(transform.position, Quaternion.AngleAxis(-45, Vector3.up) * this.transform.right * visibleDistance, Color.green);
+
+        //Raycasts
+        RaycastHit hit;
+        float forwardDist = 0, rightDist = 0, leftDist = 0, 
+              right45Dist = 0, left45Dist = 0;
+
+        //forward
+        if (Physics.Raycast(transform.position, this.transform.forward, out hit, visibleDistance))
+        {
+            // we will keep farthest value to small(0) and closest value to big(1)
+            forwardDist =1 - Round(hit.distance/visibleDistance); // normalized and rounded
+        }
+
+        //right
+        if (Physics.Raycast(transform.position, this.transform.right, out hit, visibleDistance))
+        {
+            // we will keep farthest value to small(0) and closest value to big(1)
+            rightDist = 1 - Round(hit.distance / visibleDistance); // normalized and rounded
+        }
+
+        //left
+        if (Physics.Raycast(transform.position, -this.transform.right, out hit, visibleDistance))
+        {
+            // we will keep farthest value to small(0) and closest value to big(1)
+            leftDist = 1 - Round(hit.distance / visibleDistance); // normalized and rounded
+        }
+
+        //right45
+        if (Physics.Raycast(transform.position, Quaternion.AngleAxis(-45, Vector3.up) * transform.right, out hit, visibleDistance))
+        {
+            // we will keep farthest value to small(0) and closest value to big(1)
+            right45Dist = 1 - Round(hit.distance / visibleDistance); // normalized and rounded
+        }
+
+        //left45
+        if (Physics.Raycast(transform.position, Quaternion.AngleAxis(45, Vector3.up) * -transform.right, out hit, visibleDistance))
+        {
+            // we will keep farthest value to small(0) and closest value to big(1)
+            left45Dist = 1 - Round(hit.distance / visibleDistance); // normalized and rounded
+        }
+
+        //we will feed these input to ANN
+        string trainingData = forwardDist + "," + rightDist + "," + leftDist + ","  + right45Dist + "," +
+            left45Dist + "," + Round(translationInput) + "," + Round(rotationInput);
+
+        if(!collectedTrainingData.Contains(trainingData))
+            collectedTrainingData.Add(trainingData);
+    }
+}
